498d73911686008dd675262cc036dedeb0d2f2b8
Dewashish Gope <dewashishgope99@gmail.com>
2024-11-30 21:52:32 +0530

    oops basics

--- oops.js
+++ oops.js
@@ -17,6 +17,63 @@ class mycar extends car {
 }
 
 let vehicle = new mycar("Tata","Curv");
-console.log(vehicle.start());
-console.log(vehicle.drive());
+// console.log(vehicle.start());
+// console.log(vehicle.drive());
+
+//Encapsulation
+
+class BankAccount {
+    #balance = 100;
+
+    deposit(amount){
+        this.#balance+=amount;
+        return this.#balance;
+    }
+    getBalance(){
+        return `Rs.${this.#balance}`;
+    }
+}
+
+let account = new BankAccount();
+// console.log(account.getBalance());
+
+// Abstraction
+
+class coffeeMachine{
+    start(){
+        return `Starting the Coffee Machine`;
+    }
+    brewCoffee(){
+        return `Brewing Coffee`;
+    }
+    getCoffee(){
+        let firststep = this.start();
+        let secondstep = this.brewCoffee();
+        return `${firststep}...${secondstep}`
+    }
+}
+
+let myCoffee = new coffeeMachine();
+// console.log(myCoffee.getCoffee());
+
+
+//polmorphism
+
+class bird{
+    fly(){
+        return `Hey I am a bird , and i can fly`;
+    }
+}
+
+class ostrich extends bird{
+    fly(){
+        return ` Hey I am a bird , and i can't fly`;
+    }
+}
+
+let hen = new bird()
+let Ostrich = new ostrich()
+
+console.log(hen.fly());
+console.log(Ostrich.fly());
 
